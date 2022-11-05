553e22eb1473e6753b2fa7be8f4a3dbe3e0494d1
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-11-05 14:45:03 +0100

    project initialized

---
+++ src/List.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function List() {
+  return (
+    <main>
+          
+    </main>
+  )
+}
+
+export default List
---
+++ src/data.js
@@ -0,0 +1,142 @@
+export default [
+  {
+    id: 1,
+    name: "Ruperta Vennings",
+    age: 28,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567339/users/Avatar7_ssou2s.jpg",
+  },
+  {
+    id: 2,
+    name: "Roxi Pritchard",
+    age: 31,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567336/users/Avatar17_qjhajk.jpg",
+  },
+  {
+    id: 3,
+    name: "Ursuline Gravatt",
+    age: 28,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567336/users/Avatar4_u6rpdp.jpg",
+  },
+  {
+    id: 4,
+    name: "Nathan Head",
+    age: 38,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667654613/users/Avatar33_euvbyp.jpg",
+  },
+  {
+    id: 5,
+    name: "Brewster Ottosen",
+    age: 33,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667654613/users/Avatar35_jf5ijc.jpg",
+  },
+  {
+    id: 6,
+    name: "Alberik Empringham",
+    age: 27,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667654614/users/Avatar38_kzt6lq.jpg",
+  },
+  {
+    id: 7,
+    name: "Therine Goodrick",
+    age: 29,
+    image:
+      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAACzSURBVCjPY/jPgB8yEKmg0aHxf93/6jPlR4vP5P/I+p9yFMOEuvP7/pedKerJ7cmYnNwTOx9DQaVB9/8J/3McILyw/VjcUFA//3/a/QQBPI5MOT/7f2QBHgWxCRHvg2bhVBAjEHY/8DaecAhd73/GswfCNvmPoSA4we+8x2kQywLoTP33aAqCDHzeu79xrLepN+83uq/3Xwvdm94Jrvsd9lvtN91vuF93v+Z+tX5S44ICBQA4egHkwuNCKQAAAABJRU5ErkJggg==",
+  },
+  {
+    id: 8,
+    name: "Josephine Joincey",
+    age: 27,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667654591/users/Avatar24_tsnycj.jpg",
+  },
+  {
+    id: 9,
+    name: "Berke MacKissack",
+    age: 30,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567292/users/Avatar1_mhhf5c.jpg",
+  },
+  {
+    id: 10,
+    name: "Claribel Norcop",
+    age: 39,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567306/users/Avatar2_zehqj1.jpg",
+  },
+  {
+    id: 11,
+    name: "Myrah Bolderoe",
+    age: 23,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567307/users/Avatar6_qy1zn7.jpg",
+  },
+  {
+    id: 12,
+    name: "Eduardo Bart",
+    age: 37,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567309/users/Avatar9_bfcy1a.jpg",
+  },
+  {
+    id: 13,
+    name: "Alene Abella",
+    age: 37,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567312/users/Avatar5_lsrsgq.jpg",
+  },
+  {
+    id: 14,
+    name: "Mel Feast",
+    age: 35,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567315/users/Avatar16_y038td.png",
+  },
+  {
+    id: 15,
+    name: "Mozelle Topes",
+    age: 24,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567316/users/Avatar8_zxvtd3.jpg",
+  },
+  {
+    id: 16,
+    name: "Donelle Mowen",
+    age: 26,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567316/users/Avatar3_bw9evy.jpg",
+  },
+  {
+    id: 17,
+    name: "Leena Fareweather",
+    age: 22,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567320/users/Avatar15_wfp4c5.png",
+  },
+  {
+    id: 18,
+    name: "Joelly Brickdale",
+    age: 40,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567328/users/Avatar12_ore2ga.png",
+  },
+  {
+    id: 19,
+    name: "Stesha Carsberg",
+    age: 26,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567325/users/Avatar13_legghg.png",
+  },
+  {
+    id: 20,
+    name: "Zaccaria Forsythe",
+    age: 39,
+    image:
+      "https://res.cloudinary.com/ibukunlive/image/upload/v1667567323/users/Avatar11_jxldsx.jpg",
+  },
+];
